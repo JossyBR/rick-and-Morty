1c085c8ac06d04abf5c439f2b6142eb559089e8e
Jossy Benitez <jossybr94@gmail.com>
2023-06-12 21:30:11 -0500

    Homework de testing

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,38 +1,39 @@
-const express = require("express");
-const server = express();
 const PORT = 3001;
-const router = require('./routes/routes');
+const server = require("./app");
 
-
-
-//Middlewares
-server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header(
-       'Access-Control-Allow-Headers',
-       'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.header(
-       'Access-Control-Allow-Methods',
-       'GET, POST, OPTIONS, PUT, DELETE'
-    );
-    next();
- });
-
-
- server.use(express.json());
-
- server.use('/rickandmorty', router);
-
- server.listen(PORT, () => {
-   console.log("Server raised in port: " + PORT);
- });
+server.listen(PORT, () => {
+  console.log("Server raised in port: " + PORT);
+});
 
 
 
 
 
+
+
+// const express = require("express");
+// const server = express();
+
+// const router = require("./routes/routes");
+
+// //Middlewares
+// server.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header(
+//        'Access-Control-Allow-Headers',
+//        'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     res.header(
+//        'Access-Control-Allow-Methods',
+//        'GET, POST, OPTIONS, PUT, DELETE'
+//     );
+//     next();
+//  });
+
+//  server.use(express.json());
+
+//  server.use('/rickandmorty', router);
 
 // const data = require("./utils/data.js");
 // const http = require("http");
