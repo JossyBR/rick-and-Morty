a178895aaa7ff57b07df472e67cb74f16274efbc
Jossy Benitez <jossybr94@gmail.com>
2023-11-03 20:48:37 -0500

    Corrigiendo algunas cosas

--- Client/src/components/Form/Form.jsx
+++ Client/src/components/Form/Form.jsx
@@ -1,67 +1,75 @@
-
 import React from "react";
 
 import { useState } from "react";
 import validation from "./validation";
-import styles from "./Form.module.css"
-
+import styles from "./Form.module.css";
 
 const Form = (props) => {
-    const { login } = props;
-    const [userData, setUserData] = useState({email: "", password: ""});
-    const [errors, setErrors] = useState({});
+  const { login } = props;
+  const [userData, setUserData] = useState({ email: "", password: "" });
+  const [errors, setErrors] = useState({});
 
-    const handleChange = (e) => {
-        setUserData({...userData, [e.target.name]: e.target.value});
-        // console.log(userData);
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+    // console.log(userData);
 
-        setErrors(validation({...userData, [e.target.name]: e.target.value}));
-    }
+    setErrors(validation({ ...userData, [e.target.name]: e.target.value }));
+  };
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      login(userData);
-    }
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(userData);
+  };
 
   return (
     <div className={styles.divForm}>
       <form onSubmit={handleSubmit} className={styles.form}>
         <h1 className={styles.h1Sesion}>Inciar Sesión</h1>
 
-        <label htmlFor="email" className={styles.labels}>Email</label>
-        <input 
-        type="text" id="email" placeholder="Email..." 
-        name="email" 
-        value={userData.email} onChange={handleChange}
-        className={styles.inputEmail} />
+        <label htmlFor="email" className={styles.labels}>
+          Email
+        </label>
+        <input
+          type="text"
+          id="email"
+          placeholder="Email..."
+          name="email"
+          value={userData.email}
+          onChange={handleChange}
+          className={styles.inputEmail}
+        />
 
         {/* Aqui se renderiza condicionalmente si hay un error */}
-        {
-          errors.e1 ? (<p>{errors.e1}</p>):
-          errors.e2 ? (<p>{errors.e2}</p>):
-          (<p>{errors.e3}</p>)
-        }
+        {errors.e1 ? (
+          <p className={styles.error}>{errors.e1}</p>
+        ) : errors.e2 ? (
+          <p className={styles.error}>{errors.e2}</p>
+        ) : (
+          <p className={styles.error}>{errors.e3}</p>
+        )}
 
-        <label 
-        htmlFor="password"
-        className={styles.labels}>Password</label>
-        <input 
-        type="password" 
-        id="password" 
-        placeholder="Password..." name="password" 
-        value={userData.password} 
-        onChange={handleChange}
-        className={styles.inputPass} />
+        <label htmlFor="password" className={styles.labels}>
+          Password
+        </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password..."
+          name="password"
+          value={userData.password}
+          onChange={handleChange}
+          className={styles.inputPass}
+        />
 
+        {errors.p1 ? (
+          <p className={styles.error}>{errors.p1}</p>
+        ) : (
+          <p className={styles.error}>{errors.p2}</p>
+        )}
 
-        {
-          errors.p1 ? (<p>{errors.p1}</p>):
-          (<p>{errors.p2}</p>)
-        }
-
-        <button type="submit" className={styles.loginButton}>Login</button>
+        <button type="submit" className={styles.loginButton}>
+          Login
+        </button>
       </form>
     </div>
   );
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,28 +1,47 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createRoot } from "react-dom";
+// import "./index.css";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+// // import { Store } from "redux";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+
+// // createRoot(
+// //     <BrowserRouter>
+// //       <App />
+// //     </BrowserRouter>,
+
+// //   document.getElementById('root')
+// // )
+
+// ReactDOM.render(
+
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </Provider>,
+
+//   document.getElementById("root")
+// );
+
 import React from "react";
 import ReactDOM from "react-dom";
-import { createRoot } from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import { Store } from "redux";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { createRoot } from "react-dom/client";
 
-// createRoot(
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>,
-
-//   document.getElementById('root')
-// )
-
-ReactDOM.render(
-
+const root = document.getElementById("root");
+const reactRoot = createRoot(root);
+reactRoot.render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-
-  document.getElementById("root")
+  </Provider>
 );
